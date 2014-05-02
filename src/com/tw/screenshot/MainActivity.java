1c1161cb61b07a619c5c344e5038eae6e7e9509d
striversist <striversist@qq.com>
2014-05-02 17:59:15 +0800

    【功能】1.增加ActionBar Menu“功能设置”的响应（目前为Sherlock demos中的Preferences例子）；2.Activity切换间增加动画（利用overridePendingTransition和anim配置来实现）；3. 改变Android sdk配置：4.1.2 -> 4.0

--- src/com/tw/screenshot/MainActivity.java
+++ src/com/tw/screenshot/MainActivity.java
@@ -1,10 +1,12 @@
 package com.tw.screenshot;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
 import com.tw.screenshot.adapter.MainFragmentAdapter;
 import com.tw.screenshot.fragment.HomeFragment;
 import com.viewpagerindicator.PageIndicator;
@@ -31,11 +33,41 @@ public class MainActivity extends SherlockFragmentActivity {
         mIndicator = (TabPageIndicator) findViewById(R.id.indicator);
         mIndicator.setViewPager(mPager);
     }
+    
+    @Override
+    protected void onPause() {
+        super.onPause();
+        overridePendingTransition(R.anim.slide_right_in, R.anim.slide_left_out);
+    }
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
+        overridePendingTransition(R.anim.slide_left_in, R.anim.slide_right_out);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getSupportMenuInflater().inflate(R.menu.main_menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.action_settings:
+                startActivity(new Intent(this, SettingActivity.class));
+                break;
+            case R.id.action_help:
+                break;
+            case R.id.action_feedback:
+                break;
+            case R.id.action_about:
+                break;
+            case R.id.action_exit:
+                break;
+        }
+        return true;
     }
 
 }
---
+++ src/com/tw/screenshot/SettingActivity.java
@@ -0,0 +1,28 @@
+package com.tw.screenshot;
+
+import com.actionbarsherlock.app.SherlockPreferenceActivity;
+import com.actionbarsherlock.view.MenuItem;
+
+import android.os.Bundle;
+
+public class SettingActivity extends SherlockPreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_setting);
+        
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(menuItem);
+    }
+}
